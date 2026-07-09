bf23eaad66b432ecf7366d2944d8c575c21f306d
Ashwath S <ashwathash2004@gmail.com>
2026-07-09 19:17:47 +0530

    Initial commit - PDF Study Assistant

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from pypdf import PdfReader
+from google import genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+st.title("PDF Study Assistant")
+st.write("Upload a PDF and ask questions about it")
+uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
+question = st.text_input("Ask a question about your PDF")
+if st.button("Get Answer"):
+    if uploaded_file is None:
+        st.error("Please upload a PDF first")
+    elif question == "":
+        st.error("Please type a question")
+    else:
+        reader = PdfReader(uploaded_file)
+        if len(reader.pages) > 50:
+            st.warning("Large PDF detected. Reading first 50 pages only.")
+            pages_to_read = reader.pages[:50]
+        else:
+            pages_to_read = reader.pages
+        pdf_text = ""
+        for page in pages_to_read:
+            pdf_text += page.extract_text()
+        prompt = f"""
+        Here is a document:
+        {pdf_text}
+        
+        Answer this question based only on the document:
+        {question}
+        """
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    st.subheader("Answer:")
+    st.write(response.text)
